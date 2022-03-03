81350313c30e5d5dea5885b9f76a486d92e209f9
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-03-03 13:03:52 -0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+const { TOKEN, PREFIX } = process.env;
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: new Intents(32767) });
 
@@ -6,8 +8,8 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-  if (message.content == '!help') { /* ACTION */ }
-  if (message.content == '!command') { /* ACTION */ }
+  if (message.content == `${PREFIX}help`) { /* ACTION */ }
+  if (message.content == `${PREFIX}command`) { /* ACTION */ }
 });
 
-client.login('TOKEN');
+client.login(TOKEN);
