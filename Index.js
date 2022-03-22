986dd901005619a9e516a97dd57e772ef23febd4
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-03-22 11:35:51 -0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,18 @@
+const { TOKEN, PREFIX } = process.env;
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: new Intents(32767) });
+
+client.once('ready', () => {
+  console.log(`Name: ${client.user.username}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  Arguments = message.content.slice(PREFIX.length).split(/ +/);
+  Command = Arguments.shift();
+  
+  if (Command === 'help') { /* ACTION */ }
+});
+
+client.login(TOKEN).catch(err => console.log('Start-Up Failed'));
