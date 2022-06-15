61c2643bb7bb8d0179c0bb7de6218ecb3b1bd54c
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-06-15 13:03:14 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,33 +1,52 @@
 const { TOKEN, PREFIX } = process.env;
 
 const { Client, Intents, Collection } = require('discord.js');
-const { readdirSync } = require('fs');
 const client = new Client({ intents: new Intents(32767) });
 
-client.commands = new Collection();
+const { AsciiTable3, AlignmentEnum } = require('ascii-table3');
+const FileSync = require('fs');
 
-Folder = readdirSync('./Commands/');
+client.commands = new Collection();
+var Commands = new AsciiTable3();
+
+Commands.setAlign(1, AlignmentEnum.CENTER);
+Commands.setAlign(2, AlignmentEnum.CENTER);
+
+Commands.setHeading('COMMANDS', 'STATUS');
+
+var Folder = FileSync.readdirSync('./Commands/');
 Folder.filter(file => file.endsWith('.js'));
 
-for (file of Folder ) {
-  Command = require(`./Commands/${file}`);
+for (var file of Folder ) {
+  
+  var Command = require(`./Commands/${file}`);
   client.commands.set(Command.name, Command);
+  
+  if (Command.name && Command.description) {
+    Commands.addRow(file.replace('.js', ''), '✓');
+  }
+  
+  if (!Command.name && !Command.description) {
+    Commands.addRow(file.replace('.js', ''), '✕');
+  }
+  
 }
 
 client.once('ready', () => {
   console.log(`Name: ${client.user.username}`);
+  console.log( Commands.toString() );
 });
 
 client.on('messageCreate', (message) => {
   if (message.content.indexOf(PREFIX) !== 0) return;
-
+  
   var Arguments = message.content.slice(PREFIX.length).split(/ +/);
   var Command = Arguments.shift();
   
   if (Command === 'help') {
-    client.commands.get('Help').execute(message);
+    client.commands.get('Help').execute(message, client);
   }
-  
+
 });
 
 client.login(TOKEN);
