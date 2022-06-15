c734d949435f1fb6e12fbb29554a1b44fd66634f
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-06-15 16:48:49 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,22 @@
-const { TOKEN, PREFIX } = process.env;
+const { TOKEN } = process.env;
 
 const Discord = require('discord.js');
 const Client = new Discord.Client({ intents: new Discord.Intents(32767) });
 const Ascii = require('ascii-table3');
 const FileSync = require('fs');
+
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+var Account = require('./AccountKey.json');
+
+initializeApp({
+  credential: cert(Account)
+});
+
+var Database = getFirestore();
+
+var Collection = Database.collection('GUILDS');
 
 Client.commands = new Discord.Collection();
 var Commands = new Ascii.AsciiTable3();
@@ -22,14 +35,8 @@ for (var file of Folder ) {
   var Command = require(`./Commands/${file}`);
   Client.commands.set(Command.name, Command);
   
-  if (Command.name && Command.description) {
-    Commands.addRow(file.replace('.js', ''), '✓');
-  }
-  
-  if (!Command.name && !Command.description) {
-    Commands.addRow(file.replace('.js', ''), '✕');
-  }
-  
+  if (Command.name && Command.description) { Commands.addRow(file.replace('.js', ''), '✓') }
+  if (!Command.name && !Command.description) { Commands.addRow(file.replace('.js', ''), '✕') }
 }
 
 Client.once('ready', () => {
@@ -37,16 +44,29 @@ Client.once('ready', () => {
   console.log( Commands.toString() );
 });
 
-Client.on('messageCreate', (message) => {
-  if (message.content.indexOf(PREFIX) !== 0) return;
-  
-  var Arguments = message.content.slice(PREFIX.length).split(/ +/);
-  var Command = Arguments.shift();
-  
-  if (Command === 'help') {
-    Client.commands.get('Help').execute(message, client);
-  }
+Client.on('guildCreate', (guild) => {
+  Collection.doc(guild.id).set({
+    NAME: guild.name,
+    PREFIX: '!'
+  });
+});
 
+Client.on('messageCreate', (message) => {
+  Collection.get().then(Snapshot => {
+    Snapshot.forEach(document => {
+      var Prefix = document.data().PREFIX;
+      
+      if (message.content.indexOf(Prefix) !== 0) return;
+      
+      var Arguments = message.content.slice(Prefix.length).split(/ +/);
+      var Command = Arguments.shift();
+      
+      if (Command === 'help') {
+        Client.commands.get('Help').execute(message)
+      }
+      
+    })
+  });
 });
 
 Client.login(TOKEN);
